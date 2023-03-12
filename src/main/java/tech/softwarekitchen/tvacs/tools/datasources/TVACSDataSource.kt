bc0649b00f7a3834031a5a7c92472f2df629ef85
Tahlaria <tahlaria@software-kitchen.tech>
2023-03-12 21:12:39 +0100

    CHANGED - data sources have multiple streams now

--- src/main/java/tech/softwarekitchen/tvacs/tools/datasources/TVACSDataSource.kt
+++ src/main/java/tech/softwarekitchen/tvacs/tools/datasources/TVACSDataSource.kt
@@ -1,17 +1,30 @@
 package tech.softwarekitchen.tvacs.tools.datasources
 
-import tech.softwarekitchen.tvacs.tools.datasources.type.DSTypeXYDataPoint
 import kotlin.reflect.KClass
 
-interface TVACSDataSource<T> {
+interface TVACSDataStream<T>{
     fun tick()
     fun get(): T
+    fun getName(): String
+}
+interface TVACSDataSource {
+    fun tick(){
+        getStreams().forEach(TVACSDataStream<*>::tick)
+    }
+    fun getStreams(): List<TVACSDataStream<*>>
+
+    fun getStream(name: String): TVACSDataStream<*>{
+        return getStreams().first{it.getName() == name}
+    }
 }
 
-interface TVACS2DDataPointSeriesDataSource: TVACSDataSource<DSTypeXYDataPoint>
+class TVACSStreamIdentifier(path: String){
+    val source: String = path.split("->")[0]
+    val stream: String = path.split("->")[1]
+}
 
-interface TVACSDataSourceFactory<T: TVACSDataSource<*>>{
+interface TVACSDataSourceFactory<T: TVACSDataSource>{
     fun create(): T
-    fun getType(): KClass<*>
+    fun getTypes(): Map<String, KClass<*>>
     fun getName(): String
 }
