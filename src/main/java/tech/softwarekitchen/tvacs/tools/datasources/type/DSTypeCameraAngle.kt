c05d00e051665af84e9490ed76ade31f7962a302
Tahlaria <tahlaria@software-kitchen.tech>
2023-03-25 11:27:56 +0100

    CHANGED - version to 0.4, ADDED - camera angle ds impl

---
+++ src/main/java/tech/softwarekitchen/tvacs/tools/datasources/type/DSTypeCameraAngle.kt
@@ -0,0 +1,6 @@
+package tech.softwarekitchen.tvacs.tools.datasources.type
+
+import tech.softwarekitchen.common.vector.Vector3
+
+data class DSTypeCameraAngle(val base: Vector3, val lookDirection: Vector3, val up: Vector3)
+
